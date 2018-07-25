69a6e072c4b1c068d8b434d16437a3d87b1bd207
Scott Van Gilder <svangilder@gmail.com>
2018-07-25 13:51:43 -0600

    add examples

---
+++ code-wars/src/js/example/test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+const inverseCaptcha01 = require('./example');
+
+describe('Day 1 - Part a: Inverse Captcha 2', () => {
+	it('Calculate Inverse Captcha of 1122 correctly', () => {
+		assert.equal(3, inverseCaptcha01('1122'));
+	});
+	
+	it('Calculate Inverse Captcha of 1111 correctly', () => {
+		assert.equal(4, inverseCaptcha01('1111'));
+	});
+	
+	it('Calculate Inverse Captcha of 1234 correctly', () => {    
+		assert.equal(0, inverseCaptcha01('1234'));
+	});
+	
+	it('Calculate Inverse Captcha of 91212129 correctly', () => {    
+		assert.equal(9, inverseCaptcha01('91212129'));
+	});
+});
